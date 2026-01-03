fe439cf1471cc8fb8ce8fcf744f6dd22711c0678
ismail-169 <oganlaismail305@gmail.com>
2026-01-03 23:46:45 +0100

    add railway files

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server running on port ' + PORT);
+});
